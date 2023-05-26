980ef7a5b728583817c898eefa8f03fbfa000f39
Chris Varga <chrisvarga@users.noreply.github.com>
2023-05-26 21:36:21 +0000

    Add MIT license to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="murloc",
-    version="0.0.1",
+    version="0.0.2",
     author="Chris Varga",
     author_email="",
     description="extensible api server",
@@ -15,6 +15,7 @@ setup(
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     keywords="extensible api server",

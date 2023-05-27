803ff4c339e60f9763d0d6fd9d63c11c6a22d03f
Chris Varga <chrisvarga@users.noreply.github.com>
2023-05-27 21:58:11 +0000

    Bump version in setup.py to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="murloc",
-    version="0.0.3",
+    version="0.0.4",
     author="Chris Varga",
     author_email="",
     description="extensible api server",

f3e007ad1380a79a594348e728f421a49fce3136
Chris Varga <chrisvarga@users.noreply.github.com>
2023-07-11 22:32:56 +0000

    Add uvicorn and async support

--- murloc/__init__.py
+++ murloc/__init__.py
@@ -1,1 +1,47 @@
+"""
+Murloc is an extensible api server.
+
+The Murloc api follows the following conventions:
+
+Request:  {"method": String, "params": Object}
+Response: {"error": Number, "data": Object}
+
+To define api methods, you can include them as a dict() during init:
+
+```python
+# file: main.py
+from murloc import Murloc
+
+def hello_world():
+    return "hello, world!"
+
+def echo_args(args):
+    return args
+
+app = Murloc(methods={"hello":hello_world,"echo":echo_args})
+```
+
+Or, optionally, you can also use the route decorator like so:
+
+```python
+# file: main.py
+from murloc import Murloc
+
+app = Murloc()
+
+@app.route("hello")
+def hello_world():
+    return "hello, world!"
+
+@app.route("echo")
+def echo_args(args):
+    return args
+```
+
+In either case, you can run murloc with uvicorn like so:
+
+$ uvicorn main:app
+
+Note: This assumes main.py and the Murloc variable to be `app`.
+"""
 from .murloc import Murloc
--- murloc/murloc.py
+++ murloc/murloc.py
@@ -1,184 +1,120 @@
-#!/usr/bin/env python3
+# MIT License
+#
+# Copyright (c) 2023 Chris Varga
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-import os
-import sys
-import time
 import json
-import struct
-import signal
-import socket
-import inspect
 
 
 class Murloc:
-    def __init__(
-        self,
-        version="1.0.0",
-        host=socket.inet_ntoa(struct.pack("!L", socket.INADDR_LOOPBACK)),
-        port=8048,
-        name="murloc",
-        mode="default",
-        url="Aaaaaughibbrgubugbugrguburgle!",
-        methods=dict(),
-        logfile=None,
-        verbose=False,
-    ):
-        self.version = version
-        self.host = host
-        self.port = port
-        self.name = name
-        self.mode = mode
-        self.url = url
+    """
+    Murloc is an extensible api server. See help(murloc) for more info.
+    """
+
+    def __init__(self, methods=dict()):
         self.methods = methods
-        self.logfile = logfile
-        self.verbose = verbose
-        self.pid = os.getpid()
-        self.boot = inspect.cleandoc(
-            f"""\n
-                    
-                 ___
-                /\  \ 
-               /::\  \       {self.name} {self.version}
-              /:/\:\  \ 
-             /:/  \:\  \ 
-            /:/__/ \:\__\    Running in {self.mode} mode
-            \:\  \ /:/  /    Port: {self.port:<4}
-             \:\  /:/  /     PID:  {self.pid:<4}
-              \:\/:/  / 
-               \::/  /             {self.url}
-                \/__/ 
-                    """
+
+    async def __call__(self, scope, receive, send):
+        async def read_body(receive):
+            """
+            Read and return the entire body from an incoming ASGI message.
+            """
+            body = b""
+            more_body = True
+            while more_body:
+                message = await receive()
+                body += message.get("body", b"")
+                more_body = message.get("more_body", False)
+            return body
+
+        async def parse(req):
+            """
+            Parse a json request for `method` and `params` and call handle().
+            """
+            err = {"error": 1, "data": None}
+            try:
+                js = json.loads(req)
+            except Exception as e:
+                err["data"] = "Invalid json request"
+                return json.dumps(err)
+            try:
+                method = js["method"]
+            except:
+                err["data"] = "Request missing method"
+                return json.dumps(err)
+            try:
+                params = js["params"]
+            except:
+                params = None
+            return await handle(method, params)
+
+        async def handle(method, params):
+            """
+            Call method if defined. Handle optional and missing `params` cases.
+            """
+            if method not in self.methods:
+                return json.dumps({"error": 1, "data": "Method not defined"})
+            # We could just raise an exception here if they passed params
+            # to a method not expecting them. Then they would just get an
+            # Internal Error response. But why not handle it gracefully?
+            # Similarly for if they don't pass params when they should.
+            if params:
+                try:
+                    return self.methods[method](params)
+                except:
+                    # If we fail even after giving a second chance, let it die.
+                    return self.methods[method]()
+            else:
+                try:
+                    return self.methods[method]()
+                except:
+                    return json.dumps({"error": 1, "data": "Method expected params"})
+
+        # Main logic begins here.
+        assert scope["type"] == "http"
+        body = await read_body(receive)
+        res = await parse(body)
+        await send(
+            {
+                "type": "http.response.start",
+                "status": 200,
+                "headers": [
+                    (b"content-type", b"text/plain"),
+                ],
+            }
+        )
+        await send(
+            {
+                "type": "http.response.body",
+                "body": str(res).encode(),
+            }
         )
 
-    # def __call__(self, **params):
-    #    self.handle(**params)
-
     def route(self, rule, **params):
+        """
+        This is a convenience decorator provided for easy route definitions.
+        See help(murloc) for more info.
+        """
+
         def decorator(func):
             self.methods[rule] = func
             return func
 
         return decorator
-
-    def log(self, s):
-        date = time.strftime("%Y-%m-%d %H:%M:%S")
-        if self.verbose:
-            n = inspect.currentframe().f_back.f_lineno
-            msg = f"[{date}] [{os.path.basename(__file__)}:{n}] [{os.getpid()}] {s}"
-        else:
-            msg = f"[{date}] [{os.getpid()}] {s}"
-        if not self.logfile:
-            print(msg)
-        else:
-            try:
-                with open(self.logfile, "a") as f:
-                    f.write(f"{msg}\n")
-            except:
-                print(msg)
-
-    def debug(self, s):
-        if not self.verbose:
-            return
-        date = time.strftime("%Y-%m-%d %H:%M:%S")
-        n = inspect.currentframe().f_back.f_lineno
-        msg = f"[{date}] [{os.path.basename(__file__)}:{n}] [{os.getpid()}] {s}"
-        if not self.logfile:
-            print(msg)
-        else:
-            try:
-                with open(self.logfile, "a") as f:
-                    f.write(f"{msg}\n")
-            except:
-                print(msg)
-
-    def handle(self, method, params):
-        err = {"err": 1, "data": "method not defined"}
-        if method not in self.methods:
-            self.debug(f"method {method} not defined")
-            return json.dumps(err)
-        if params:
-            return self.methods[method](params)
-        else:
-            try:
-                return self.methods[method]()
-            except:
-                err = {"err": 1, "data": "missing parameters"}
-                return json.dumps(err)
-
-    def parse(self, req):
-        err = {"err": 1, "data": None}
-        try:
-            js = json.loads(req)
-        except Exception as e:
-            self.debug(f"json.loads: {req}: {e}")
-            err["data"] = "invalid json request"
-            return json.dumps(err)
-        try:
-            method = js["method"]
-        except:
-            err["data"] = "request lacks method"
-            return json.dumps(err)
-        try:
-            params = js["params"]
-        except:
-            params = None
-        return self.handle(method, params)
-
-    def recvall(self, conn):
-        data = b""
-        while True:
-            packet = conn.recv(1024)
-            data += packet
-            if len(packet) < 1024:
-                break
-        return data
-
-    def handle_connection(self, conn, addr):
-        while True:
-            try:
-                data = self.recvall(conn)
-            except Exception as e:
-                self.debug(f"recv: {e}")
-                conn.shutdown(socket.SHUT_RDWR)
-                conn.close()
-                break
-            if not data:
-                conn.shutdown(socket.SHUT_RDWR)
-                conn.close()
-                break
-            req = data.decode().strip()
-            res = f"{self.parse(req)}\n"
-            conn.sendall(res.encode())
-
-    def handle_sigint(self, sig, frame):
-        print()
-        sys.exit(0)
-
-    def handle_sigchild(self, sig, frame):
-        os.waitpid(-1, os.WNOHANG)
-
-    def serve(self):
-        self.log(self.boot)
-        signal.signal(signal.SIGINT, self.handle_sigint)
-        signal.signal(signal.SIGCHLD, self.handle_sigchild)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind((self.host, self.port))
-        sock.listen()
-        self.log(f"Listening at {self.host}:{self.port}")
-        while True:
-            try:
-                conn, addr = sock.accept()
-            except Exception as e:
-                self.debug(f"accept: {e}")
-                break
-            conn.settimeout(10)
-            # Fork to handle the new connection; this allows us to use
-            # os.system() etc, which is problematic within a thread.
-            pid = os.fork()
-            if pid == 0:
-                self.debug(f"Connection from {addr}")
-                self.handle_connection(conn, addr)
-                sys.exit(0)
-            else:
-                continue
